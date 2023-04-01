0268eb051d4565890a3548ed00571922db796c5e
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2023-04-01 23:50:26 +1100

    Update Server.java

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,24 +1,21 @@
+package Server;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import javax.swing.JOptionPane;
 
 public class Server {
 	static int port;
+	static String path;
 	public static void main(String[] args) {
 		try {
 			ServerSocket serveSocket = new ServerSocket(port);
 			while(true) {
 				Socket s = serveSocket.accept();
-				Thread thread = new Thread(() -> 
-				{
-	
-						serveClient(s,filepath);
-					
-				});
-				t.start();
+				Thread thread = new Thread(() -> {startClient(s, path);});
+				thread.start();
 			}
 		}
 		catch (IOException e) 
@@ -28,5 +25,8 @@ public class Server {
 		
 		
 	}
-
+	
+	public static void startClient(Socket clientSocket,String path) {
+		
+	}
 }

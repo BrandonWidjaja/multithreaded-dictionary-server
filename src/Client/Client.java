84d6bc709e667eeaeef1bc129b0a0e2ef773dab6
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2023-04-02 18:26:34 +1000

    some implementation of multithreading

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,10 +1,46 @@
 package Client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String ip = args[0];
+		int port = Integer.parseInt(args[1]);
+		try(Socket socket = new Socket(ip, port);){
+			System.out.println("Connection established");
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+			
+			Scanner scanner = new Scanner(System.in);
+			String inputStr = null;
+			//While the user input differs from "exit"
+			while (!"exit".equalsIgnoreCase(inputStr))
+			{
+				inputStr = scanner.nextLine();
+				out.write(inputStr + "\n");
+				out.flush();
+				
+				System.out.println("Message sent");
+				
+				String received = in.readLine(); 
+				
+				System.out.println("Message received: " + received);
+				in.close();
+				out.close();
+				socket.close();
+			}
+			scanner.close();
+		}
+		catch(Exception e) {
+			
+		}
+	
 	}
-
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,20 +1,33 @@
 package Server;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import javax.swing.JOptionPane;
 
 public class Server {
+	
 	static int port;
 	static String path;
+	
 	public static void main(String[] args) {
+		try {
+			port = Integer.parseInt(args[0]);
+			path = args[1];
+			
+		}
+		catch (Exception e) {
+			
+		}
 		try {
 			ServerSocket serveSocket = new ServerSocket(port);
 			while(true) {
 				Socket s = serveSocket.accept();
 				Thread thread = new Thread(() -> {startClient(s, path);});
+				System.out.println("thread created");
 				thread.start();
 			}
 		}
@@ -26,7 +39,23 @@ public class Server {
 		
 	}
 	
-	public static void startClient(Socket clientSocket,String path) {
-		
+	public static void startClient(Socket cSocket,String path) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(cSocket.getInputStream(), "UTF-8"));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(cSocket.getOutputStream(), "UTF-8"));
+			
+			String inputString = null;
+			
+			while ((inputString = reader.readLine()) != null) {
+				System.out.printf(
+                        " Sent from the client: %s\n",
+                        inputString);
+                    
+                    System.out.println("ack sent back");
+			}
+			
+		}catch (Exception e) {
+			
+		}
 	}
 }
